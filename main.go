ff3088dbd27024b78571f8c0db8af15e0b5218e0
Zev Solomon <zev2888@gmail.com>
2024-05-08 13:56:24 +0900

    Temporary broken upload

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -23,6 +27,11 @@ func getCommandsList() map[string]cliCommand {
 			name:        "exit",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "Obtain a list of locations",
+			callback:    commandMap,
 		},
 	}
 }
@@ -45,6 +54,50 @@ Usage:
 
 func commandExit() error {
 	os.Exit(0)
+	return nil
+}
+type PokeLocs struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+var prevUrl string
+var nextUrl string
+func commandMap() error {
+	url := "https://pokeapi.co/api/v2/location/"
+	if nextUrl != nil {
+		url = nextUrl
+	}
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	locs := PokeLocs{}
+	jsonErr := json.Unmarshal(body, &locs)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	prevUrl = locs.Previous
+	nextUrl = locs.Next
 	return nil
 }
 
